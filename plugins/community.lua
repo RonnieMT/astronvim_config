83b7ec0917d878d0e98e29167b339886586c79b5
RonnieMT <ronnieronton@me.com>
2023-04-07 15:17:29 -0600

    changed the files

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,10 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.tokyonight" },
+  { import = "astrocommunity.colorscheme.nightfox" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.syntax.vim-easy-align" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.java" },
 }
